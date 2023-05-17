83fa9719411f57a34c7f514ba7f55c434188153c
Thomas Furay <103867568+TFuray@users.noreply.github.com>
2023-05-17 14:47:54 -0700

    updated user controller for login logout update user

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv"
 import express from "express"
 import connectDB from "./config/db.js"
+import cookieParser from "cookie-parser"
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js"
 import userRoutes from "./routes/userRoutes.js"
 dotenv.config()
@@ -12,6 +13,8 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+app.use(cookieParser())
 
 app.use("/api/users", userRoutes)
 
